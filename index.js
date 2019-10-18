d7fdd6c24b378e43b65234bd59b12aa5f4adb978
Hank <hankyu.p@gmail.com>
2019-10-18 19:19:19 +0900

    * ancestorCount가 작은 것을 먼저 사용하도록 함

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-var accumulative = require('./accumulative')
-var blackjack = require('./blackjack')
-var utils = require('./utils')
+var accumulative = require("./accumulative");
+var blackjack = require("./blackjack");
+var utils = require("./utils");
+
+function utxoAncestorCountScore(x) {
+    return x.ancestorCount || 0;
+}
 
 // order by descending value, minus the inputs approximate fee
-function utxoScore (x, feeRate) {
-  return x.value - utils.feeAmount(utils.inputBytes(x), feeRate)
+function utxoAmountScore(x, feeRate) {
+    return x.value - utils.feeAmount(utils.inputBytes(x), feeRate);
 }
 
-module.exports = function coinSelect (utxos, outputs, feeRate) {
-  utxos = utxos.concat().sort(function (a, b) {
-    return utxoScore(b, feeRate) - utxoScore(a, feeRate)
-  })
+module.exports = function coinSelect(utxos, outputs, feeRate) {
+    utxos = utxos.concat().sort(function(a, b) {
+        const ancestorScore = utxoAncestorCountScore(b) - utxoAncestorCountScore(a);
+        if (ancestorScore == 0) return utxoAmountScore(b, feeRate) - utxoAmountScore(a, feeRate);
+        else return ancestorScore;
+    });
 
-  // attempt to use the blackjack strategy first (no change output)
-  var base = blackjack(utxos, outputs, feeRate)
-  if (base.inputs) return base
+    // attempt to use the blackjack strategy first (no change output)
+    var base = blackjack(utxos, outputs, feeRate);
+    if (base.inputs) return base;
 
-  // else, try the accumulative strategy
-  return accumulative(utxos, outputs, feeRate)
-}
+    // else, try the accumulative strategy
+    return accumulative(utxos, outputs, feeRate);
+};
